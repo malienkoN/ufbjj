dc89cda27711f739c9310768aa7b996b5b7e6259
malienkoN <nekmalienko2001@gmail.com>
2021-06-16 12:12:14 +0300

    rating ready desktop

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,24 @@
-window.addEventListener('load', function () {
+$(document).ready(function () {
+    // tabs
+    $('.rating__triggers-item').click(function (e) {
+        e.preventDefault();
 
+        $('.rating__triggers-item').removeClass('rating__triggers-item--active');
+        $('.rating__content-item').removeClass('rating__content-item--active');
+
+        $(this).addClass('rating__triggers-item--active');
+        $($(this).attr('href')).addClass('rating__content-item--active');
+    });
+
+    $('.rating__triggers-item:first').click();
+
+    // table select
+    $('.rating').hide();
+    $('.t1').show();
+
+    $('.rating-select').on('change', function () {
+        $('.rating').hide();
+        $('.t' + $(this).val()).show();
+        $('.t' + $(this).val() + ' .rating__triggers-item:first').click();
+    }).val("1");
 });
